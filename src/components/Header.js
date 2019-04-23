fca69c1b6d99ac695312a633d8b9e3a27eb45ba2
gull-iintellect <gull.iintellect@gmail.com>
2019-04-24 04:45:33 +0500

    Header Changes, with uper and lower Navigation Bar.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,30 +2,93 @@ import React from 'react';
 import { Link } from 'gatsby';
 import MetaData from './MetaData';
 
+
 export default () => (
     <React.Fragment>
         <MetaData />
-        <div className="columns is-mobile">
-            <div className="column" style={{marginBottom: '40px'}}>
-                <h2><Link to="/">Logo here</Link></h2>
-            </div>
-            <div className="column dropdown-cont" style={{backgroundColor: 'rgb(131, 131, 132)'}}>
-                <ul className="container dropdown-ul" style={{listStyle: 'none', marginBottom: 'unset'}}>
-                    <li className="dropdown"><Link to="/about">About</Link>
-                        <ul className="dropdown-list" style={{listStyle: 'none'}}>
-                            <li><a href="#"> Alpha 1 </a></li>
-                            <li><a href="#"> Alpha 2 </a></li>
-                            <li><a href="#"> Alpha 3 </a></li>
-                            <li><a href="#"> Alpha 4 </a></li>
-                        </ul>
-                    </li>
-                    <li><Link to="/blog">Blog</Link></li>
-                    <li><Link to="/about">Contact</Link></li>
-                    <li><Link to="/blog">Blog</Link></li>
-                </ul>
-            </div>
+        
+ 
+          <div className="columns is-mobile">
+     <div className="column dropdown-cont1" style={{backgroundColor: '#313131'}}>
+             <ul className="container dropdown-ul1" style={{listStyle: 'none', marginBottom: 'unset', marginTop: '0'}}>
+                 <li className="dropdown"><Link to="/about">About</Link></li>
+                 <li><Link to="/about">Contacts</Link></li>
+                 <li><Link to="/blog">Advertise</Link></li>
+             </ul>
+             <div className="right-nav">
+             <div className="header-icons">
+             <a><i className="fa fa-facebook"></i></a>
+             <a><i className="fa fa-twitter"></i></a>
+             <a><i className="fa fa-google-plus"></i></a>
+             <a><i className="fa fa-instagram"></i></a>
+             <a><i className="fa fa-pinterest"></i></a>
+             </div>
+             <div className="right-nav-butt">
+                <button class="login-button">LOG IN</button>
+             </div>
+             <hr className="right-nav-hr"></hr>
+             <div className="date-container">
+                <div><h3>23</h3></div>
+                <div className="date-cont-p">
+                    <p>APR</p>
+                    <p>2019</p>
+                </div>
+             </div>
+             </div>
+         </div>
+         <div className="column">
+             <h2><Link to="/">Logo here</Link></h2>
+         </div> 
+         <hr className="logo-bottom-line"></hr>
+          <div className="column dropdown-cont" >
+             <ul className="container dropdown-ul" style={{listStyle: 'none', marginBottom: 'unset'}}>
+                 <li className="dropdown nav-below col"><Link to="/about">HOME</Link>
+                 <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
+                     <ul className="dropdown-list" style={{listStyle: 'none'}}>
+                         <li><a href="#"> Alpha 1 </a></li>
+                         <li><a href="#"> Alpha 2 </a></li>
+                         <li><a href="#"> Alpha 3 </a></li>
+                         <li><a href="#"> Alpha 4 </a></li>
+                     </ul>
+                 </li>
+                 <li className="dropdown nav-below col-1"><Link to="/about">FEATURES</Link>
+                 <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
+                     <ul className="dropdown-list" style={{listStyle: 'none'}}>
+                         <li><a href="#"> Alpha 1 </a></li>
+                         <li><a href="#"> Alpha 2 </a></li>
+                         <li><a href="#"> Alpha 3 </a></li>
+                         <li><a href="#"> Alpha 4 </a></li>
+                     </ul>
+                 </li>
+                 <li className="dropdown nav-below col-1"><Link to="/about">BLOG STYLES</Link>
+                 <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
+                     <ul className="dropdown-list" style={{listStyle: 'none'}}>
+                         <li><a href="#"> Alpha 1 </a></li>
+                         <li><a href="#"> Alpha 2 </a></li>
+                         <li><a href="#"> Alpha 3 </a></li>
+                         <li><a href="#"> Alpha 4 </a></li>
+                     </ul>
+                 </li>
+                 <li className="dropdown nav-below col-1"><Link to="/about">MEGA MENU</Link>
+                 <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
+                     <ul className="dropdown-list" style={{listStyle: 'none'}}>
+                         <li><a href="#"> Alpha 1 </a></li>
+                         <li><a href="#"> Alpha 2 </a></li>
+                         <li><a href="#"> Alpha 3 </a></li>
+                         <li><a href="#"> Alpha 4 </a></li>
+                     </ul>
+                 </li>
+                 <li className="dropdown nav-below col-1"><Link to="/about">SHOP</Link>
+                 <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
+                     <ul className="dropdown-list" style={{listStyle: 'none'}}>
+                         <li><a href="#"> Alpha 1 </a></li>
+                         <li><a href="#"> Alpha 2 </a></li>
+                         <li><a href="#"> Alpha 3 </a></li>
+                         <li><a href="#"> Alpha 4 </a></li>
+                     </ul>
+                 </li>
+             </ul>
+         </div> 
         </div>
     </React.Fragment>
 )
-// , marginLeft: '-25%', marginRight: '-25%', paddingLeft: '25%', paddingRight: '25%'
-// style={{float: 'left'}}
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import 'font-awesome/css/font-awesome.min.css';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Titillium Web:300,400,700', 'Montserrat']
+  }
+});
 
 export default ({ children }) => (
     <div className="container-fluid">
